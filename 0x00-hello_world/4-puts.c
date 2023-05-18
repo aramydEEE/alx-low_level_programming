c5b5c1c9a7ff9092354601e58f5f9e3c72e743b6
Morufat <aramydeee@gmail.com>
2023-05-18 14:12:16 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	 char x[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(x);
+	return (0);
+}
