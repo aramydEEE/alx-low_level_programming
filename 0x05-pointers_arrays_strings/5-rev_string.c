15ac6751c2f4f169d01388b19370f544c0b4d0a7
Morufat <aramydeee@gmail.com>
2023-06-02 11:45:00 +0000

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,19 +8,16 @@
 
 void print_rev(char *s)
 {
-	int i, r, n;
-	char *x;
+	int i, r;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		;
 	}
 	r = i - 1;
-	n = i - 1;
 
 	for (; r >= 0; r--)
 	{
-		x[n] = '\0';
-		x[n] = x[n] + putchar(s[r]);
+		s = _putchar(s[r]);
 	}
 }
