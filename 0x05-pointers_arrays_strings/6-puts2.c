7e274f0e18d87d64c7fc995e1f1a339a0a451372
Morufat <aramydeee@gmail.com>
2023-06-05 20:41:20 +0000

    half the lies they tell

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - function that prints every other character of a string
+ * @str: Argument to the function
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
+
